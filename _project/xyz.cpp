7bdf29d3479f74c16d5cb1295fd4d799bd39c9b4
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-05-08 10:20:01 +0200

    test_lj_direct_summation.cpp
    Added license to source files and fixed bug in xyz.cpp

--- _project/xyz.cpp
+++ _project/xyz.cpp
@@ -1,6 +1,27 @@
-//
-// Created by pastewka on 17.04.21.
-//
+/*
+* Copyright 2021 Lars Pastewka
+*           2021 wolfram.noehring@imtek.uni-freiburg.de
+*
+* ### MIT license
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
 
 #include <iomanip>
 #include <iostream>
@@ -67,7 +88,7 @@ std::tuple<Names_t, Positions_t, Velocities_t> read_xyz_with_velocities(std::str
         for (int i = 0; i < nb_atoms; ++i) {
             std::string tmp;
             file >> names[i] >> positions(0, i) >> positions(1, i) >> positions(2, i)
-                 >> velocities(0, i) >> velocities(1, i) >> velocities(3, i);
+                 >> velocities(0, i) >> velocities(1, i) >> velocities(2, i);
             std::getline(file, tmp);
         }
 
